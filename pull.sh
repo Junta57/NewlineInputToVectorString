c57476470a5e7fe149ff866951cd49b8ae924e19
Thomas Nielsen <thomas.nielsen98@gmail.com>
2021-09-09 21:28:18 +0200

    Delete pull.sh

--- pull.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-
-echo "\nIKT103"
-cd /Users/thomasnie/ikt103g21v/assignments; git pull
-cd /Users/thomasnie/ikt103g21v/examples; git pull
-
-echo "\nIKT104"
-cd /Users/thomasnie/ikt104/examples; git pull
